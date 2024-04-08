c1bcd834e7a558cbbb99ec86dacfc911e7fcf4e6
Micah Eckard <lumberjacksoul339@gmail.com>
2024-04-07 20:56:04 -0400

    finished up front page basics

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,20 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
-import { ImMug } from "react-icons/im";
-import { TiStarburst } from "react-icons/ti";
+import { ImMug } from 'react-icons/im'
+import { TiStarburst } from 'react-icons/ti'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
-      <div className='banner'>
-        <h1>Dope Mugs</h1>
-        <TiStarburst className='circle' />
-        <ImMug className='mug' />
-      </div>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <section>
+        <div className="banner">
+          <h1>Dope Mugs</h1>
+          <TiStarburst className="circle" />
+          <ImMug className="mug" />
+        </div>
+        <h2>Showing you the very dopest mugs</h2>
+      </section>
     </>
   )
 }
