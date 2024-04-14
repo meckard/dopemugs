af67d9537682b11dfd67bccf2887ffd2db8cae5f
Micah Eckard <lumberjacksoul339@gmail.com>
2024-04-13 21:26:18 -0400

    another front page redesign

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,17 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 import { ImMug } from 'react-icons/im'
-import { TiStarburst } from 'react-icons/ti'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
-      <div className='stripe'></div>
       <section>
         <div className="banner">
-          <h1>Dope Mugs</h1>
-          <TiStarburst className="circle" />
           <ImMug className="mug" />
+          <h1>Dope Mugs</h1>
+          <h2>Showing you the very dopest mugs</h2>
         </div>
-        <h2>Showing you the very dopest mugs</h2>
       </section>
     </>
   )
