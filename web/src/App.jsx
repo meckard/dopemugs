6861ee18be95cd1f93cdc5cab0100642041fd4dd
Micah Eckard <lumberjacksoul339@gmail.com>
2024-04-20 21:06:47 -0400

    added nav animations

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,6 +4,11 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/');
+
 import './index.css'
 
 const App = () => (
--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -1,24 +1,52 @@
 import { Link, routes } from '@redwoodjs/router'
-import logo from './dope-mugs-high-resolution-logo-transparent.png'
+import { useEffect } from 'react'
+import SlAnimation from '@shoelace-style/shoelace/dist/react/animation/index'
+
 
 const Header = () => {
+
+  useEffect(() => {
+    const animation = document.getElementById('home')
+    const animation2 = document.getElementById('mugs')
+    const animation3 = document.getElementById('submit')
+
+    animation.addEventListener('mouseenter', () => {
+      animation.play = true
+    })
+
+    animation2.addEventListener('mouseenter', () => {
+      animation2.play = true
+    })
+
+    animation3.addEventListener('mouseenter', () => {
+      animation3.play = true
+    })
+
+  },[])
+
   return (
     <header>
-        <nav>
-          <p className='logo'>DM</p>
-          <ul>
+      <nav>
+        <p className="logo">DM</p>
+        <ul className="link-list">
+          <SlAnimation id='home' name='bounce' iterations='1' className='hover'>
             <Link to={routes.home()}>
-              <li>Home</li>
+              <li className='link'>Home</li>
             </Link>
-            <Link to=''>
+          </SlAnimation>
+          <SlAnimation id='mugs' name='bounce' iterations='1' className='hover'>
+            <Link to="">
               <li>Show Me the Mugs!</li>
             </Link>
-            <Link to=''>
-              <li>Submit a Mug</li>
-            </Link>
-          </ul>
-        </nav>
-      </header>
+          </SlAnimation>
+          <SlAnimation id='submit' name='bounce' iterations='1' className='hover'>
+          <Link to="">
+            <li>Submit a Mug</li>
+          </Link>
+          </SlAnimation>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
